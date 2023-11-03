d04c1a4a6d892869c5c901ba9cbd2f328a888290
ejbunnell <96508853+ejbunnell@users.noreply.github.com>
2023-11-03 08:34:49 -0400

    Started Piece.cs

---
+++ Piece.cs
@@ -0,0 +1,46 @@
+
+public class Piece {
+    public static enum PieceType {
+        Pawn,
+        Bishop,
+        Knight,
+        Rook,
+        Queen,
+        King
+    }
+    
+    public static enum PieceColor {
+        White,
+        Black
+    }
+
+    PieceType type;
+    Vector2 pos;
+    PieceColor color;
+    GameObject object;
+
+    public Piece(PieceType type, Vector2 pos, PieceColor color) {
+        this.type = type;
+        this.pos = pos;
+        this.color = color;
+        object = new GameObject();
+        
+    }
+
+    private string getSpriteName() {
+        return color + type;
+    }
+}
+
+public class Pawn : Piece {
+    bool hasMoved;
+
+    public Pawn(Vector2 pos, PieceColor color) : base(PieceType.Pawn, pos, color) {
+        hasMoved = false;
+    }
+
+    public void getLegalMoves(Piece[][] board) {
+
+    }
+}
+
